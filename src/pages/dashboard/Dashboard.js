561976d3eadad357e967a1032fd99682ac2a163a
harsh-purecode <harsh.tiwari@purecodemarketplace.io>
2024-02-23 18:11:40 +0530

    slm-10

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -48,14 +48,22 @@ function Dashboard() {
   return (
     <>
       <PageTitle>Dashboard</PageTitle>
-
-      <ProjectStarCTA />
-
+      <PageTitle>Charts</PageTitle>
+      <div className="grid gap-6 mb-8 md:grid-cols-2">
+        <ChartCard title="Traffic">
+          <Line {...lineOptions} />
+          <ChartLegend legends={lineLegends} />
+        </ChartCard>
+        <ChartCard title="Revenue">
+          <Doughnut {...doughnutOptions} />
+          <ChartLegend legends={doughnutLegends} />
+        </ChartCard>
+      </div>
       {/* <!-- Cards --> */}
       <div className="grid gap-6 mb-8 md:grid-cols-2 xl:grid-cols-4">
         <InfoCard title="Total clients" value="6389">
           <RoundIcon
-            icon={PeopleIcon}
+            icon={PeopleIcon}                                                                                                                                                                                                                     
             iconColorClass="text-orange-500 dark:text-orange-100"
             bgColorClass="bg-orange-100 dark:bg-orange-500"
             className="mr-4"
@@ -89,8 +97,8 @@ function Dashboard() {
           />
         </InfoCard>
       </div>
-
-      <TableContainer>
+      <PageTitle>Table</PageTitle>
+      <TableContainer className="mb-10">
         <Table>
           <TableHeader>
             <tr>
@@ -134,19 +142,7 @@ function Dashboard() {
           />
         </TableFooter>
       </TableContainer>
-
-      <PageTitle>Charts</PageTitle>
-      <div className="grid gap-6 mb-8 md:grid-cols-2">
-        <ChartCard title="Revenue">
-          <Doughnut {...doughnutOptions} />
-          <ChartLegend legends={doughnutLegends} />
-        </ChartCard>
-
-        <ChartCard title="Traffic">
-          <Line {...lineOptions} />
-          <ChartLegend legends={lineLegends} />
-        </ChartCard>
-      </div>
+      <ProjectStarCTA />
     </>
   )
 }
