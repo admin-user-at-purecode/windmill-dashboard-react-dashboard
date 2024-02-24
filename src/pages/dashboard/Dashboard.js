0081850e77c6f02ed84a8891ff68d63b7ef86748
harsh-purecode <159994935+harsh-purecode@users.noreply.github.com>
2024-02-24 16:59:34 +0530

    cd_2done

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from 'react'
 
-import InfoCard, { ProjectStarCTA } from '../../components/cards/InfoCard'
+import { ProjectStarCTA } from '../../components/cards/ProjectStarCTA'
+
 import ChartCard, { ChartLegend } from '../../components/chart/ChartCard'
 import { Doughnut, Line } from 'react-chartjs-2'
 import PageTitle, { RoundIcon } from '../../components/common/PageTitle'
-import { ChatIcon, CartIcon, MoneyIcon, PeopleIcon } from '../../assets/icons'
 import response from '../../mock_data/tableData'
 import {
   TableBody,
@@ -50,46 +50,6 @@ function Dashboard() {
       <PageTitle>Dashboard</PageTitle>
 
       <ProjectStarCTA />
-
-      {/* <!-- Cards --> */}
-      <div className="grid gap-6 mb-8 md:grid-cols-2 xl:grid-cols-4">
-        <InfoCard title="Total clients" value="6389">
-          <RoundIcon
-            icon={PeopleIcon}
-            iconColorClass="text-orange-500 dark:text-orange-100"
-            bgColorClass="bg-orange-100 dark:bg-orange-500"
-            className="mr-4"
-          />
-        </InfoCard>
-
-        <InfoCard title="Account balance" value="$ 46,760.89">
-          <RoundIcon
-            icon={MoneyIcon}
-            iconColorClass="text-green-500 dark:text-green-100"
-            bgColorClass="bg-green-100 dark:bg-green-500"
-            className="mr-4"
-          />
-        </InfoCard>
-
-        <InfoCard title="New sales" value="376">
-          <RoundIcon
-            icon={CartIcon}
-            iconColorClass="text-blue-500 dark:text-blue-100"
-            bgColorClass="bg-blue-100 dark:bg-blue-500"
-            className="mr-4"
-          />
-        </InfoCard>
-
-        <InfoCard title="Pending contacts" value="35">
-          <RoundIcon
-            icon={ChatIcon}
-            iconColorClass="text-teal-500 dark:text-teal-100"
-            bgColorClass="bg-teal-100 dark:bg-teal-500"
-            className="mr-4"
-          />
-        </InfoCard>
-      </div>
-
       <TableContainer>
         <Table>
           <TableHeader>
@@ -126,7 +86,7 @@ function Dashboard() {
           </TableBody>
         </Table>
         <TableFooter>
-          <Pagination
+          <Pagination 
             totalResults={totalResults}
             resultsPerPage={resultsPerPage}
             label="Table navigation"
