528645bd0de97796db5c4b16b6c3f4101d902c65
mahendhar-varma <7093741662mahiv@gmail.com>
2024-03-11 11:32:41 +0530

    ca_7

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -17,6 +17,8 @@ import {
   Avatar,
   Badge,
   Pagination,
+  Card,
+  CardBody
 } from '@windmill/react-ui'
 
 import {
@@ -134,6 +136,32 @@ function Dashboard() {
           />
         </TableFooter>
       </TableContainer>
+
+      <PageTitle>Cards with title</PageTitle>
+
+      <div className="grid gap-6 mb-8 md:grid-cols-2">
+        <Card className="cardWithHeading">
+          <CardBody>
+            <p className="mb-4 font-semibold text-gray-600 dark:text-gray-300">Revenue</p>
+            <p className="text-gray-600 dark:text-gray-400">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fuga, cum commodi a omnis
+              numquam quod? Totam exercitationem quos hic ipsam at qui cum numquam, sed amet
+              ratione! Ratione, nihil dolorum.
+            </p>
+          </CardBody>
+        </Card>
+
+        <Card colored className="text-white bg-purple-600 cardWithHeading">
+          <CardBody>
+            <p className="mb-4 font-semibold">Colored card</p>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fuga, cum commodi a omnis
+              numquam quod? Totam exercitationem quos hic ipsam at qui cum numquam, sed amet
+              ratione! Ratione, nihil dolorum.
+            </p>
+          </CardBody>
+        </Card>
+      </div>
 
       <PageTitle>Charts</PageTitle>
       <div className="grid gap-6 mb-8 md:grid-cols-2">
