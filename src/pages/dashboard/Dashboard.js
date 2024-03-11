c55f7ed784143e222438f10ebdea23a25eb02da0
mahendhar-varma <7093741662mahiv@gmail.com>
2024-03-11 13:20:33 +0530

    la_33

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -17,6 +17,7 @@ import {
   Avatar,
   Badge,
   Pagination,
+  Label
 } from '@windmill/react-ui'
 
 import {
@@ -50,6 +51,36 @@ function Dashboard() {
       <PageTitle>Dashboard</PageTitle>
 
       <ProjectStarCTA />
+
+      <div className="px-4 py-3 mb-8 bg-white rounded-lg shadow-md dark:bg-gray-800 inputWithIconContainer">
+        <Label className="inputWithIcon">
+          <span>Icon left</span>
+          {/* <!-- focus-within sets the color for the icon when input is focused --> */}
+          <div className="relative text-gray-500 focus-within:text-purple-600 dark:focus-within:text-purple-400">
+            <input
+              className="block w-full pl-10 mt-1 text-sm text-black dark:text-gray-300 dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:focus:shadow-outline-gray form-input"
+              placeholder="Jane Doe"
+            />
+            <div className="absolute inset-y-0 flex items-center ml-3 pointer-events-none">
+              <PeopleIcon  className="w-5 h-5" aria-hidden="true" />
+            </div>
+          </div>
+        </Label>
+
+        <Label className="mt-4 inputWithIcon">
+          <span className="text-gray-700 dark:text-gray-400">Icon right</span>
+          {/* <!-- focus-within sets the color for the icon when input is focused --> */}
+          <div className="relative text-gray-500 focus-within:text-purple-600 dark:focus-within:text-purple-400">
+            <input
+              className="block w-full pr-10 mt-1 text-sm text-black dark:text-gray-300 dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:focus:shadow-outline-gray form-input"
+              placeholder="Jane Doe"
+            />
+            <div className="absolute inset-y-0 right-0 flex items-center mr-3 pointer-events-none">
+              < PeopleIcon  className="w-5 h-5" aria-hidden="true" />
+            </div>
+          </div>
+        </Label>
+      </div>
 
       {/* <!-- Cards --> */}
       <div className="grid gap-6 mb-8 md:grid-cols-2 xl:grid-cols-4">
@@ -147,6 +178,9 @@ function Dashboard() {
           <ChartLegend legends={lineLegends} />
         </ChartCard>
       </div>
+
+
+
     </>
   )
 }
